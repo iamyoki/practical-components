a7533cd293252fa5fc7020b18d96dd28b242598f
Yoki Yu <yokiyuqian@gmail.com>
2021-03-27 19:15:59 +0800

    feat: add reach rouer

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { RouteComponentProps } from '@reach/router'
+
+const HomePage = (props: RouteComponentProps) => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default HomePage
